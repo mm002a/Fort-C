d3e2b05f8e5961b568cb4a13e095290827533556
Chris Thompson <chris.thompson@stfc.ac.uk>
2018-08-29 14:41:11 +0100

    Updated the Fortran codeZ

--- Prog.f90
+++ Prog.f90
@@ -4,4 +4,12 @@
         PRINT *, 'A squared:',A**2
         PI = 4*ATAN(1.0)
         PRINT *, 'PI:',PI
+        Call SR(A)
+        Print *, 'Final value of A:',A
+        END
+        Subroutine SR(Arg)
+! Just a test
+        WRITE(*,*)'IN SUBROUTINE SR'
+        Arg = Arg+1
+        Return
         END
