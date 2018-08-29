629a016bee7a1a7cd48f022b4c5661bb62f28767
Chris Thompson <chris.thompson@stfc.ac.uk>
2018-08-29 23:23:30 +0100

    Updated C code - need to do travis!

--- prog.c
+++ prog.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include <math.h>
 
+float A, pie;
+ float func(float X);
 int main() {
    /* my first program in C */
-   printf("Hello, World! \n");
-   
+   A = 1.5;
+   printf("Hello, World!- A: %d\n",A);
+   pie = func(A);
+   printf("pie: %E \n",pie);
+   pie = 4.0*atan(1.0);
+   printf("pi: %E \n",pie);
    return 0;
+}
+float func(float B)
+             {
+   float C;
+
+   printf("B: %E \n", B);
+   C = 1.2*B;
+   return C;
 }
